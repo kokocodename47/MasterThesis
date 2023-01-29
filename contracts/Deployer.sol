a6523779ced9950e8a41fd0e78930f6cbf3dbddb
kokocodename47 <kokocodename47@yahoo.com>
2023-01-29 11:50:45 +0200

    29 jan

--- contracts/Deployer.sol
+++ contracts/Deployer.sol
@@ -10,5 +10,16 @@ import "./PatchsTransactions.sol";
 contract Deployer is Regulator, Users, Drugs, PatchsTransactions {
     constructor() {
         Owner = msg.sender;
+        //RegisterDeployer();
+        UserAddressMapping[msg.sender] = User(
+            "0",
+            "Regulator",
+            UserRoles.Regulator,
+            Statuses.Active,
+            0,
+            0,
+            msg.sender
+        );
+        UsersAddresses.push(msg.sender);
     }
 }
